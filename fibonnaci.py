9329c533d9a00f7abfa2303be3ac5243ec179b9b
rekleS <kagan.usta10@gmail.com>
2023-11-04 12:33:59 +0300

    Fibonnaci Dizisi

---
+++ fibonnaci.py
@@ -0,0 +1,14 @@
+n= int(input("Kaçıncı Değeri Bulunsun: "))
+say = 0 
+x = 0
+y=1
+a=3
+print("1.Değer: ",x)
+print("2.Değer: " ,y)
+while say < (n-2) :
+    z=y+x
+    print(str(a)+"."+"Değer: ", z)
+    x=y
+    y=z
+    say += 1
+    a+=1
