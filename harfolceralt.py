ebf8ed0afa48da04fb38257341313416c89627dc
Kağan <kagan.usta10@gmail.com>
2024-03-04 17:34:45 +0300

    Update harfolceralt.py

--- harfolceralt.py
+++ harfolceralt.py
@@ -6,7 +6,5 @@ def seslikontrol(kelime):
         if karakter in harf:
             sayac+= 1
     return sayac     
-
 deger = seslikontrol(input)
 print("Sesli harf sayısı:",deger)
-#assert seslikontrol("GARDAŞ") == 2, 'Hatalı'
