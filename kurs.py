0098cc7d03a4cb6c14abf8a64f959f8b459dd0b5
Kağan <kagan.usta10@gmail.com>
2024-03-04 19:29:28 +0300

    A
    ders deneme

--- kurs.py
+++ kurs.py
@@ -1,9 +1,1 @@
-n = int(input("Sınıf kaç kişi: "))
-s = 0
-for a in range(0,n):
-    b=(float(input("Notu giriniz:")))
-    s += b
-ort = s/n 
-print("Sınıf ortalaması:",ort)
-    
-         
+import numpy as np
